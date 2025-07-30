70d1d3f569e2dd332715a21fc43592210f8375f8
kaustav-sol <kaustavrajkalita9925@gmail.com>
2025-07-30 20:22:53 +0530

    Navbar added and Page8 created, added Bootstrap and necessary Components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import Page3 from './design/Page3'
+import React from "react";
+import Page3 from "./design/Page3";
+import Page8 from "./design/Page8";
+import AppNavbar from "./components/Navbar";
 
 function App() {
-
-  
   return (
     <div className="App">
+      <AppNavbar />
       <Page3 />
+      <Page8 />
     </div>
   );
 }
