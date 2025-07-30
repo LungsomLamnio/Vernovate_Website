7fce26ed3d57a625eeaba140085192c4ee684e38
kaustav-sol <kaustavrajkalita9925@gmail.com>
2025-07-30 22:46:21 +0530

    Sushil Madhesia - Page 1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Page3 from "./design/Page3";
 import Page8 from "./design/Page8";
+import Page1 from "./design/Page1";
 import AppNavbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <AppNavbar />
+      <Page1 />
       <Page3 />
       <Page8 />
     </div>
